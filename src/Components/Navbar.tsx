e654eca7e0f2903b0816abada5d2c8a34b21eb03
Uchenna99 <uchennaagbu@gmail.com>
2025-07-02 22:36:28 +0100

    responsiveness

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,20 +1,26 @@
 import logo from "../assets/Images/logo.svg"
 import NavLink from "./NavLink";
+import burger from "../assets/Images/burger.svg"
+import close from "../assets/Images/close.svg"
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
   return (
     <>
-        <div className="w-full h-22 bg-white flex justify-center fixed top-0 left-0 z-50 px-5">
+        <div className="w-full h-22 bg-white/90 flex justify-center fixed top-0 left-0 z-50 px-5">
 
-          <div className="w-full xl:w-[1250px] h-full flex justify-between">
+          <div className="w-full xl:w-[1250px] h-full flex justify-between items-center">
 
             <div className="w-[140px] h-full flex items-center gap-3">
               <img src={logo} alt="logo" className="w-9" />
-              <p className="text-xl text-[#101828] font-semibold">Invoyce</p>
+              <p className="text-xl text-[#0a0a0a] font-semibold">Invoyce</p>
             </div>
 
-            <div className="w-fit h-full flex items-center gap-10 pt-2">
+
+            <div className="w-fit h-full hidden md:flex items-center gap-10 pt-2">
 
               <NavLink linkText="Features" onLinkClick={()=>{}} />
               <NavLink linkText="Pricing" onLinkClick={()=>{}} />
@@ -22,7 +28,8 @@ const Navbar = () => {
 
             </div>
 
-            <div className="w-fit h-full flex items-center gap-3">
+
+            <div className="w-fit h-full hidden md:flex items-center gap-3">
 
               <div className="w-fit h-[45px] flex items-center px-4 border border-[#d1d5db] rounded-xl cursor-pointer
                 hover:bg-[#737373]/10 transition-all duration-200">
@@ -36,9 +43,50 @@ const Navbar = () => {
 
             </div>
 
+
+            <div className="w-fit h-fit flex md:hidden cursor-pointer" onClick={()=> setOpenMenu(!openMenu)}>
+              {
+                !openMenu &&
+                <img src={burger} alt="" />
+              }
+              {
+                openMenu &&
+                <img src={close} alt="" />
+              }
+            </div>
+
           </div>
 
         </div>
+
+
+        <motion.div className="w-full h-fit flex flex-col gap-3 fixed top-22 left-0 z-40 bg-white p-5"
+          initial={{opacity:0, y:-200}} animate={openMenu? {opacity:1, y:0}:{}} transition={{duration:0.2}} >
+
+          <div className="w-full h-fit flex flex-col gap-5 border-b border-[#e6e6e6] pb-5">
+
+            <div className="w-full h-8 flex px-4 rounded-lg hover:bg-[#737373]/10">
+              <p className="text-sm text-[#0a0a0a] font-medium">Features</p>
+            </div>
+            <div className="w-full h-7 flex px-4 rounded-lg hover:bg-[#737373]/10">
+              <p className="text-sm text-[#0a0a0a] font-medium">Pricing</p>
+            </div>
+            <div className="w-full h-7 flex px-4 rounded-lg hover:bg-[#737373]/10">
+              <p className="text-sm text-[#0a0a0a] font-medium">FAQ</p>
+            </div>
+
+          </div>
+
+
+          <div className="w-full h-10 border border-[#e6e6e6] rounded-xl flex items-center justify-center hover:bg-[#737373]/10 mt-1">
+            <p className="text-[#0a0a0a] font-medium">Sign In</p>
+          </div>
+
+          <div className="w-full h-10 bg-[#285cb4] rounded-xl flex items-center justify-center hover:bg-[#285cb4]/90">
+            <p className="text-white font-medium">Get Started Free</p>
+          </div>
+
+        </motion.div>
     </>
   )
 }
