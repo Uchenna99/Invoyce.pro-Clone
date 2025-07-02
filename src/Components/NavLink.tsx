b7fe48360eae5d3e16a0d99689ca273cb7e84000
Uchenna99 <uchennaagbu@gmail.com>
2025-07-02 12:29:45 +0100

    laptop navbar done

---
+++ src/Components/NavLink.tsx
@@ -0,0 +1,28 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+interface Props {
+    linkText: string;
+}
+
+const NavLink = ({ linkText }: Props) => {
+    const [highlight, setHighlight] = useState(false);
+
+  return (
+    <>
+        <div className="w-fit h-fit flex flex-col gap-1 px-1 cursor-pointer"
+            onMouseEnter={()=> setHighlight(true)} onMouseLeave={()=> setHighlight(false)}>
+
+            <p className={`text-[${highlight? '#101828':'#737373'}] text-sm font-[500] transition-all duration-200`}> 
+                {linkText} 
+            </p>
+
+            <motion.div className="h-[3px] min-h-[3px] bg-[#285cb4]" 
+                initial={{width:0}} animate={highlight? {width:'100%'}:{}} transition={{duration:0.3, ease:'easeOut'}}
+            />
+        </div>
+    </>
+  )
+}
+
+export default NavLink;
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,16 +1,41 @@
-
+import logo from "../assets/Images/logo.svg"
+import NavLink from "./NavLink";
 
 
 const Navbar = () => {
   return (
     <>
-        <div className="w-full h-16 bg-white flex justify-center sticky top-0">
+        <div className="w-full h-22 bg-white flex justify-center sticky top-0">
 
-          <div className="w-full sm:w-[1300px] h-full bg-amber-300 flex justify-between">
+          <div className="w-full sm:w-[1200px] h-full flex justify-between">
 
-            <div className="w-[200px] h-full flex">
-              <img src="" alt="" />
+            <div className="w-[130px] h-full flex items-center gap-2">
+              <img src={logo} alt="logo" />
+              <p className="text-xl text-[#101828] font-semibold">Invoyce</p>
             </div>
+
+            <div className="w-fit h-full flex items-center gap-10 pt-2">
+
+              <NavLink linkText="Features" />
+              <NavLink linkText="Pricing" />
+              <NavLink linkText="FAQ" />
+
+            </div>
+
+            <div className="w-fit h-full flex items-center gap-3">
+
+              <div className="w-fit h-[45px] flex items-center px-4 border border-[#d1d5db] rounded-xl cursor-pointer
+                hover:bg-[#737373]/10 transition-all duration-200">
+                <p className="font-[500]">Sign In</p>
+              </div>
+
+              <div className="w-fit h-[42px] flex items-center px-4 border border-[#d1d5db] rounded-xl bg-[#285cb4] cursor-pointer
+                hover:bg-[#285cb4]/90 transition-all duration-200">
+                <p className="font-[500] text-white">Get Started Free</p>
+              </div>
+
+            </div>
+
           </div>
 
         </div>
