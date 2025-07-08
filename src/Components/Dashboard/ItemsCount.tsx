c81449a730fefa04971e058a49015a40c407497c
Uchenna99 <uchennaagbu@gmail.com>
2025-07-08 18:24:56 +0100

    working on invoice section

---
+++ src/Components/Dashboard/ItemsCount.tsx
@@ -0,0 +1,29 @@
+interface Props {
+    name: string;
+    count: number;
+    selected: string;
+    onClick: (filter: string)=>void;
+}
+
+const ItemsCount = ({ name, selected, count, onClick }:Props) => {
+  return (
+    <>
+        <div className={`min-w-fit h-8 flex gap-3.5 items-center border rounded-lg shadow-xs px-3 cursor-default 
+            ${selected === name? 'border-transparent bg-[#285cb4] hover:bg-[#285cb4]/90' : 'border-[#e5e5e5] hover:bg-[#f5f5f5]'}`}
+            onClick={()=> onClick(name)}>
+
+            <p className={`${selected === name? 'text-white' : 'text-[#0a0a0a]'} text-sm font-medium`}>
+                {name}
+            </p>
+
+            <div className="flex items-center justify-center bg-[#285cb4]/10 px-2 py-0.5 rounded-lg">
+                <p className="text-xs text-[#0a0a0a] font-medium">
+                    {count}
+                </p>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default ItemsCount;
--- src/Pages/UserInvoicesPage.tsx
+++ src/Pages/UserInvoicesPage.tsx
@@ -1,10 +1,80 @@
+import { FilePlus, Search } from "lucide-react";
+import ItemsCount from "../Components/Dashboard/ItemsCount";
+import { useState } from "react";
 
 
 
 const UserInvoicesPage = () => {
+  const [selectedFilter, setSelectedFilter] = useState('All');
+
   return (
     <>
-        <p>Invoices</p>
+        <div className="w-full h-fit flex flex-col py-4 gap-6">
+
+          <div className="w-full h-fit flex justify-between items-center">
+
+            <div className="w-fit flex flex-col ">
+              <h2 className="text-[#0a0a0a] text-2xl font-bold tracking-tight mb-1">
+                Invoices
+              </h2>
+              <p className="text-[#737373] text-sm">
+                Manage your invoices
+              </p>
+            </div>
+
+            <button className="w-fit h-9 px-3 flex items-center gap-4 bg-[#285cb4] hover:bg-[#285cb4]/90 text-white font-medium 
+              justify-center rounded-lg text-sm text-nowrap">
+              <FilePlus size={16} />
+              Create Invoice
+            </button>
+
+          </div>
+
+
+          <div className="w-full h-fit flex flex-col gap-4">
+            <div className="w-full h-fit flex items-center gap-2">
+
+              <ItemsCount 
+                name="All" selected={selectedFilter} count={0}
+                onClick={(filterChange)=> setSelectedFilter(filterChange)}
+              />
+              <ItemsCount 
+                name="Draft" selected={selectedFilter} count={0}
+                onClick={(filterChange)=> setSelectedFilter(filterChange)}
+              />
+              <ItemsCount 
+                name="Sent" selected={selectedFilter} count={0}
+                onClick={(filterChange)=> setSelectedFilter(filterChange)}
+              />
+              <ItemsCount 
+                name="Paid" selected={selectedFilter} count={0}
+                onClick={(filterChange)=> setSelectedFilter(filterChange)}
+              />
+              <ItemsCount 
+                name="Overdue" selected={selectedFilter} count={0}
+                onClick={(filterChange)=> setSelectedFilter(filterChange)}
+              />
+              <ItemsCount 
+                name="Cancelled" selected={selectedFilter} count={0}
+                onClick={(filterChange)=> setSelectedFilter(filterChange)}
+              />
+
+            </div>
+
+            <div className="w-112 h-9 relative flex items-center">
+              <Search size={16} color="#99a1af" className="absolute left-3 mt-1"/>
+              <input type="text" placeholder="Search invoices..."
+                className="w-full h-full rounded-lg border-none pl-10 pr-4 text-sm text-[#0a0a0a] placeholder-[#737373] shadow-sm 
+                transition-all duration-300 outline outline-[#e5e5e5] focus:outline-[#285cb4] focus:ring-4 focus:ring-[#285cb4]/50"
+              />
+            </div>
+          </div>
+
+
+
+          {/* <YourClients/> */}
+
+        </div>
     </>
   )
 }
